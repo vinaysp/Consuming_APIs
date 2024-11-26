14a9876218bb27a5637092f7e5f75b7b3f585528
Vinay Patel <vinayspatel0@gmail.com>
2024-11-26 21:18:24 +0000

    refactor: create RequestParams in apiResults method

--- src/main/java/org/northcoders/dao/FakeBooksDAO.java
+++ src/main/java/org/northcoders/dao/FakeBooksDAO.java
@@ -1,20 +1,22 @@
 package org.northcoders.dao;
 
 import org.northcoders.model.Results;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class FakeBooksDAO {
 
-   public static Results apiResults(){
+   public static Results apiResults(@RequestParam String localeValue, @RequestParam int quantityValue, @RequestParam int seedValue){
        String BASE_URL ="https://fakerapi.it/api/v1/books";
        WebClient webClient = WebClient.builder().baseUrl(BASE_URL).build();
 
        Results responseBody;
 
+
        responseBody = webClient
                .get()
-               .uri("?_locale=en_GB&_quantity=5&_seed=20")
+               .uri("?_locale="+localeValue+"&_quantity="+quantityValue+"&_seed="+seedValue)
                .retrieve()
                .bodyToMono(Results.class)
                .block();
